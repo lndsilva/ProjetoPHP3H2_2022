4f93c12a0ca2018befa7099121ffdf1b7c08a75c
lndsilva <lndsilva@hotmail.com>
2022-08-15 11:50:09 -0300

    construtor e destrutor

---
+++ aula02/Conta_Cons_Destr.php
@@ -0,0 +1,59 @@
+<?php 
+
+	class Conta
+	{
+		var $agencia;
+		var $codigo;
+		var $dataDeCriacao;
+		var $titular;
+		var $senha;
+		var $saldo;
+		var $cancelada;
+		
+		//diminui o saldo em quantia
+		function retirar($quantia)
+		{
+			if ($quantia > 0) {
+				$this->saldo-=$quantia;
+			}
+		}
+
+		//aumenta o saldo em quantia
+		function depositar($quantia){
+			if ($quantia > 0) {
+				$this->saldo+=$quantia;
+			}
+		}
+
+		
+		//retorna o saldo atual
+		function obterSaldo(){
+			return $this->saldo;
+		}
+		
+		//método construtor inicializa propriedades
+		function __construct($agencia, $codigo, $dataDeCriacao, $titular, $senha, $saldo){
+			$this->agencia = $agencia;
+			$this->codigo = $codigo;
+			$this->dataDeCriacao = $dataDeCriacao;
+			$this->titular = $titular;
+			$this->senha = $senha;
+			
+			//chamada a outro método da classe
+			$this->depositar($saldo);
+			$this->cancelada = false;
+		}
+
+		//método destrutor finaliza objeto
+		function __destruct(){
+			echo "Objeto Conta {$this->codigo} de {$this->titular->nome} finalizada ... <br>";
+		}
+
+
+
+
+
+	}
+
+ ?>
+ 
---
+++ aula02/Pessoa_Cons_Destr.php
@@ -0,0 +1,54 @@
+<?php
+
+class Pessoa
+{
+	var $codigo;
+	var $nome;
+	var $altura;
+	var $idade;
+	var $nascimento;
+	var $escolaridade;
+	var $salario;
+	
+	//aumenta a altura em centimetros
+	function crescer($centimetros)
+	{
+		if ($centimetros > 0) {
+			$this->altura += $centimetros;
+		}
+	}
+
+	//altera a escolaridade para titulação
+	function formar($titulacao){
+		$this->escolaridade = $titulacao;
+	}
+	//aumenta a idade em anos
+	function envelhecer($anos){
+		if ($anos > 0) {
+			$this->idade += $anos;
+		}
+	}
+
+	//método construtor inicializa propriedades
+
+	function __construct($codigo, $nome, $altura, $idade, $nascimento, $escolaridade, $salario)
+	{
+		$this->codigo = $codigo;
+		$this->nome = $nome;
+		$this->altura = $altura;
+		$this->idade = $idade;
+		$this->nascimento = $nascimento;
+		$this->escolaridade = $escolaridade;
+		$this->salario = $salario;
+
+		}
+
+	//método destrutor finaliza Objeto
+
+	function __destruct(){
+
+		echo "Objeto {$this->Nome} finalizado ... <br>";
+	}
+}
+
+?>
